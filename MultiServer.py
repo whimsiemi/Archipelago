647946f2cf37d99976753dc72a9b678b7a18d4de
LotsaTella <mochanitrate@gmail.com>
2025-08-25 00:27:25 +0100

    ping cmd for testing

--- MultiServer.py
+++ MultiServer.py
@@ -2014,7 +2014,10 @@ async def process_client_cmd(ctx: Context, client: Client, args: dict):
                                                          set(bounceclient.tags) & tags or
                                                          bounceclient.slot in slots):
                     await ctx.send_encoded_msgs(bounceclient, msg)
-
+        elif cmd == "Ping":
+            args["cmd"] = "Pong"
+            msg = ctx.dumper([args])
+            await ctx.send_msgs(client, msg)
         elif cmd == "Get":
             if "keys" not in args or type(args["keys"]) != list:
                 await ctx.send_msgs(client, [{'cmd': 'InvalidPacket', "type": "arguments",
